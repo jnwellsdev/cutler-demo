66b35023861bf35940548aa0a20689818bbd7352
Jesse Wells <jwells@renegadecommunications.com>
2023-03-17 00:51:00 -0400

    test build

---
+++ .output/public/_nuxt/composables.3b9b7e07.js
@@ -0,0 +1,1 @@
+import{a}from"./entry.a3f183fb.js";function s(e,u){return a()._useHead(e,u)}export{s as u};
--- .output/public/_payload.js
+++ .output/public/_payload.js
@@ -1,1 +1,1 @@
-export default {data:{},prerenderedAt:1678990710562}
+export default {data:{},prerenderedAt:1679028653751}
