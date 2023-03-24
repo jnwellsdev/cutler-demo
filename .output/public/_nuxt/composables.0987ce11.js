16333e416bef0a9b6684ac59bb6b16ad06edb2d4
Ken Hall <khall@renegadecommunications.com>
2023-03-24 15:01:58 -0400

    updated game questinos

---
+++ .output/public/_nuxt/composables.0987ce11.js
@@ -0,0 +1,1 @@
+import{a}from"./entry.85944c06.js";function s(e,u){return a()._useHead(e,u)}export{s as u};
--- .output/public/_payload.js
+++ .output/public/_payload.js
@@ -1,1 +1,1 @@
-export default {data:{},prerenderedAt:1679672376896}
+export default {data:{},prerenderedAt:1679684502880}
